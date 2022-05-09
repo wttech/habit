65078525b8a71154acaf5e11e164b59746aa92b6
Tomasz Krug <tomasz.krug@gmail.com>
2022-05-09 18:46:56 +0200

    Disabling Gradle plugin publication to sonatype

--- gradle-plugin/build.gradle.kts
+++ gradle-plugin/build.gradle.kts
@@ -59,6 +59,18 @@ tasks.withType<Test> {
 }
 
 tasks {
+    val publishPluginMavenPublicationToSonatypeRepository by existing
+
+    publishPluginMavenPublicationToSonatypeRepository {
+        enabled = false
+    }
+
+    val publishGradlePluginPluginMarkerMavenPublicationToSonatypeRepository by existing
+
+    publishGradlePluginPluginMarkerMavenPublicationToSonatypeRepository {
+        enabled = false
+    }
+
     val generateProperties by existing
 
     jar {
